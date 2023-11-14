a2b94c89d7880f9fb0f66ccc2c0a9396a8c77fd2
Amirreza Noori <amirreza.noori@yahoo.com>
2023-11-14 19:39:22 +0330

    initd

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+
+// JSX
+const myHello = <div>
+    Salaam React
+    <p>Sal<b>aam</b></p>
+</div>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(myHello)
